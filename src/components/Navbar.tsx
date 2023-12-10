01076206df6727702617474baa47cff2da5c9761
Julius Moeckel <julius.moeckel@web.de>
2023-12-10 21:54:48 +0100

    Anpassungen NavBar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,38 @@
-const Navigation = () => {
-    return (
-      <div>
-        <nav className = "nav">
-            <a href = "/" className ="Logo"> LOGO </a>
-            <ul>
-                <li>
-                    <a href = "/buchsuchen"> Buchsuchen </a>
-                    <a href = "/buchanlegen"> Buchanlegen </a>
-                    <a href = "/anmelden"> Login </a>
-                </li>
-            </ul>
-        </nav>
-     </div>
-    )
-}
+import { Box, Flex, Text, Link, Spacer } from '@chakra-ui/react';
 
-export default Navigation
+const Navigation = () => {
+  return (
+    <Flex
+      p={4}
+      align="center"
+      justify="space-between"
+      bg="gray.400"
+      color="white"
+    >
+      <Box>
+        <Link href="/" fontSize="xl" fontWeight="bold">
+            LOGO
+        </Link>
+      </Box>
+
+      <Spacer />
+
+      <Flex>
+            <Link href="/buchsuchen" mr={4}>
+                BuchSuchen
+            </Link>
+            <Link href="/buchanlegen" mr={4}> 
+            BuchAnlegen
+            </Link>
+      </Flex>
+
+        <Spacer />
+
+        <Link href="/anmelden">
+            Login
+        </Link>
+    </Flex>
+  );
+};
+
+export default Navigation;
