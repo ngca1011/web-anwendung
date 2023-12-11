ef6508859c15e5c4bb3b84dafab1be8eff41f6de
Julius Moeckel <julius.moeckel@web.de>
2023-12-11 17:55:35 +0100

    style: BuchCreatePage erweitern

--- src/pages/BuchCreatePage.tsx
+++ src/pages/BuchCreatePage.tsx
@@ -1,7 +1,103 @@
-export default function Buchsuchen() {
+import {
+    Table,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+    TableContainer,
+    Box,
+    FormControl,
+    Input,
+    Button
+  } from '@chakra-ui/react'
+  
+  export default function Buchanlegen() {
     return (
-        <div>
-            BUCH ANLEGEN PAGE
-        </div>
+      <div>
+        <TableContainer>
+          <Table variant='simple'>
+            <Thead>
+              <Tr>
+                <Th>Kriterium</Th>
+                <Th>Eingabe</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td>Titel</Td>
+                <Td>
+                  <Box maxW="300px">
+                    <FormControl>
+                      <Input />
+                    </FormControl>
+                  </Box>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>ISBN Nummer</Td>
+                <Td>
+                  <Box maxW="300px">
+                    <FormControl>
+                      <Input />
+                    </FormControl>
+                  </Box>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>Buchart</Td>
+                <Td>
+                  <Box maxW="300px">
+                    <FormControl>
+                      <Input />
+                    </FormControl>
+                  </Box>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>Preis</Td>
+                <Td>
+                  <Box maxW="300px">
+                    <FormControl>
+                      <Input />
+                    </FormControl>
+                  </Box>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>Rabatt</Td>
+                <Td>
+                  <Box maxW="300px">
+                    <FormControl>
+                      <Input />
+                    </FormControl>
+                  </Box>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>Homepage</Td>
+                <Td>
+                  <Box maxW="300px">
+                    <FormControl>
+                      <Input />
+                    </FormControl>
+                  </Box>
+                </Td>
+              </Tr>
+            </Tbody>
+            <Tfoot>
+            </Tfoot>
+          </Table>
+        </TableContainer>
+
+        <Box display="flex" justifyContent="center" alignItems="center" marginBottom="4">
+            <Button colorScheme="gray">Anlegen</Button>
+        </Box>
+
+
+      </div>
     )
-}
+  }
+  
--- src/pages/BuchSuchenPage.tsx
+++ src/pages/BuchSuchenPage.tsx
@@ -1,139 +1,232 @@
-import {  FormHelperText, Input, RangeSlider, Radio, RadioGroup, Stack, Switch, FormControl, FormLabel,
-    RangeSliderTrack, NumberInput, Table,
-    Thead,Tbody  ,Tr ,Th ,Td ,
-    TableCaption, TableContainer,
-    NumberInputField,NumberInputStepper,NumberIncrementStepper,
-    NumberDecrementStepper, Box, RangeSliderFilledTrack, RangeSliderThumb, } from "@chakra-ui/react"
-
-    const currentYear = new Date().getFullYear();
-
-export default function Buchsuchen() {
+import {
+    FormHelperText,
+    Input,
+    RangeSlider,
+    Radio,
+    RadioGroup,
+    Stack,
+    Switch,
+    FormControl,
+    FormLabel,
+    RangeSliderTrack,
+    NumberInput,
+    Table,
+    Thead,
+    Tbody,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+    TableContainer,
+    NumberInputField,
+    NumberInputStepper,
+    NumberIncrementStepper,
+    Button,
+    NumberDecrementStepper,
+    Box,
+    RangeSliderFilledTrack,
+    RangeSliderThumb,
+  } from "@chakra-ui/react";
+  
+  const currentYear = new Date().getFullYear();
+  
+  export default function Buchsuchen() {
     return (
-       <div>
+      <div>
         <TableContainer>
-<Table variant='simple'>
-  <TableCaption>Suchkriterien</TableCaption>
-  <Thead>
-    <Tr>
-      <Th>Kriterium</Th>
-      <Th>Anpassen</Th>
-    </Tr>
-  </Thead>
-  <Tbody>
-    <Tr>
-      <Td>Titel</Td>
-      <Td>
-      <Box mb={4} maxW="300px">
-        <FormControl>
-        <Input/>
-        </FormControl>
-        </Box>
-      </Td>
-    </Tr>
-    <Tr>
-      <Td>ISBN Number</Td>
-      <Td>
-      <Box mb={4} maxW="300px">
-        <FormControl>
-        <Input/>
-        <FormHelperText>Example: "9780131969452"</FormHelperText>
-        </FormControl>
-        </Box>
-      </Td>
-    </Tr>
-    <Tr>
-      <Td>Rating</Td>
-      <Td>
-      <Box mb={4} maxW="300px">
-            <RangeSlider aria-label={['min', 'max']} defaultValue={[0, 10]}>
-                <RangeSliderTrack>
-                    <RangeSliderFilledTrack />
-                </RangeSliderTrack>
-                <RangeSliderThumb index={0} />
-                <RangeSliderThumb index={1} />
-            </RangeSlider>
-            </Box>
-      </Td>
-    </Tr>
-    <Tr>
-        <Td>Buchart</Td>
-        <Td>
-        <Box mb={4}>
-            <RadioGroup>
-                <Stack direction='row'>
-                    <Radio value='1'>Druckausgabe</Radio>
-                    <Radio value='2'>Kindle</Radio>
-                </Stack>
-            </RadioGroup>
-            </Box>
+          <Table variant="simple">
+            <Thead>
+              <Tr>
+                <Th>Kriterium</Th>
+                <Th>Anpassen</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td>Titel</Td>
+                <Td>
+                  <Box mb={4} maxW="300px">
+                    <FormControl>
+                      <Input />
+                    </FormControl>
+                  </Box>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>ISBN Number</Td>
+                <Td>
+                  <Box mb={4} maxW="300px">
+                    <FormControl>
+                      <Input />
+                      <FormHelperText>Example: "9780131969452"</FormHelperText>
+                    </FormControl>
+                  </Box>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>Rating</Td>
+                <Td>
+                  <Box mb={4} maxW="300px">
+                    <RangeSlider
+                      aria-label={["min", "max"]}
+                      defaultValue={[0, 10]}
+                    >
+                      <RangeSliderTrack>
+                        <RangeSliderFilledTrack />
+                      </RangeSliderTrack>
+                      <RangeSliderThumb index={0} />
+                      <RangeSliderThumb index={1} />
+                    </RangeSlider>
+                  </Box>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>Buchart</Td>
+                <Td>
+                  <Box mb={4}>
+                    <RadioGroup>
+                      <Stack direction="row">
+                        <Radio value="1">Druckausgabe</Radio>
+                        <Radio value="2">Kindle</Radio>
+                      </Stack>
+                    </RadioGroup>
+                  </Box>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>Preis</Td>
+                <Td>
+                  <Box maxW="300px">
+                    <RangeSlider
+                      aria-label={["min", "max"]}
+                      defaultValue={[0, 10]}
+                    >
+                      <RangeSliderTrack>
+                        <RangeSliderFilledTrack />
+                      </RangeSliderTrack>
+                      <RangeSliderThumb index={0} />
+                      <RangeSliderThumb index={1} />
+                    </RangeSlider>
+                  </Box>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>Rabatt</Td>
+                <Td>
+                  <Box maxW="300px">
+                    <RangeSlider
+                      aria-label={["min", "max"]}
+                      defaultValue={[0, 10]}
+                    >
+                      <RangeSliderTrack>
+                        <RangeSliderFilledTrack />
+                      </RangeSliderTrack>
+                      <RangeSliderThumb index={0} />
+                      <RangeSliderThumb index={1} />
+                    </RangeSlider>
+                  </Box>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>Lieferbar</Td>
+                <Td>
+                  <FormControl display="flex" alignItems="center">
+                    <FormLabel mb="0"></FormLabel>
+                    <Switch id="lieferbar" />
+                  </FormControl>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>Erscheinungsjahr</Td>
+                <Td>
+                  <Box maxW="300px">
+                    <NumberInput
+                      defaultValue={currentYear}
+                      min={1800}
+                      max={currentYear}
+                    >
+                      <NumberInputField />
+                      <NumberInputStepper>
+                        <NumberIncrementStepper />
+                        <NumberDecrementStepper />
+                      </NumberInputStepper>
+                    </NumberInput>
+                  </Box>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>Homepage</Td>
+                <Td>
+                  <Box maxW="300px">
+                    <FormControl>
+                      <Input />
+                    </FormControl>
+                  </Box>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </TableContainer>
 
-        </Td>
+        <Box display="flex" justifyContent="center" alignItems="center" marginBottom="4">
+      <Button colorScheme="gray">Suchen</Button>
+    </Box>
+
+    <TableContainer>
+  <Table variant='striped' colorScheme='gray'>
+    <TableCaption>Gefundene Bücher</TableCaption>
+    <Thead>
+      <Tr>
+        <Th>Titel</Th>
+        <Th>ISBN Nummer</Th>
+        <Th>Rating</Th>
+        <Th>Buchart</Th>
+        <Th>Preis</Th>
+        <Th>Rabatt</Th>
+        <Th>Lieferbar</Th>
+        <Th>Erscheinungsjahr</Th>
+        <Th>Homepage</Th>
+      </Tr>
+    </Thead>
+    <Tbody>
+      <Tr>
+        <Td></Td>
+        <Td></Td>
+        <Td></Td>
+        <Td></Td>
+        <Td></Td>
+        <Td></Td>
+        <Td></Td>
+        <Td></Td>
+        <Td></Td>
       </Tr>
       <Tr>
-        <Td>Preis</Td>
-        <Td>
-        <Box maxW="300px">
-            <RangeSlider aria-label={['min', 'max']} defaultValue={[0, 10]}>
-                <RangeSliderTrack>
-                    <RangeSliderFilledTrack />
-                </RangeSliderTrack>
-                <RangeSliderThumb index={0} />
-                <RangeSliderThumb index={1} />
-            </RangeSlider>
-            </Box>
-        </Td>
+        
+      <Td></Td>
+        <Td></Td>
+        <Td></Td>
+        <Td></Td>
+        <Td></Td>
+        <Td></Td>
+        <Td></Td>
+        <Td></Td>
+        <Td></Td>
       </Tr>
       <Tr>
-        <Td>Rabatt</Td>
-        <Td>
-            <Box maxW="300px">
-            <RangeSlider aria-label={['min', 'max']} defaultValue={[0, 10]}>
-                <RangeSliderTrack>
-                    <RangeSliderFilledTrack />
-                </RangeSliderTrack>
-                <RangeSliderThumb index={0} />
-                <RangeSliderThumb index={1} />
-            </RangeSlider>
-            </Box></Td>
+        <Td></Td>
+        <Td></Td>
+        <Td></Td>
+        <Td></Td>
+        <Td></Td>
+        <Td></Td>
+        <Td></Td>
+        <Td></Td>
+        <Td></Td>
       </Tr>
-      <Tr>
-        <Td>Lieferbar</Td>
-        <Td>
-            <FormControl display='flex' alignItems='center'>
-                 <FormLabel mb='0'>
-                </FormLabel>
-                <Switch id='lieferbar' />
-            </FormControl></Td>
-      </Tr>
-      <Tr>
-        <Td>Erscheinungsjahr</Td>
-        <Td>
-        <Box maxW="300px">
-            <NumberInput defaultValue={currentYear} min={1800} max={currentYear}>
-            <NumberInputField />
-            <NumberInputStepper>
-            <NumberIncrementStepper />
-            <NumberDecrementStepper />
-            </NumberInputStepper>
-            </NumberInput>
-            </Box>
-        </Td>
-      </Tr>
-      <Tr>
-        <Td>Homepage</Td>
-        <Td>
-        <Box maxW="300px">
-        <FormControl>
-        <Input/>
-        </FormControl>
-        </Box>
-        </Td>
-      </Tr>
-  </Tbody>
-</Table>
+    </Tbody>
+  </Table>
 </TableContainer>
 
-        </div>
-
-)
-}
+    </div>
+    );
+  }
