a127a644873029eebcd1705973cacc868c6b9865
Cao Nguyen Duong Nguyen <caond.nguyen@gmail.com>
2023-12-12 20:59:39 +0100

    Loginseite Fetch von NavBar

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,49 +1,68 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useState } from "react";
 import axios from 'axios';
+import { Center, Box, Heading, Button, Input, useToast } from '@chakra-ui/react';
 
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const toast = useToast();
 
-  const login = async () => {
+  const handleLogin = async () => {
     try {
-        const response = await axios.post('http://localhost:3000/api/auth/login', {
-            username,
-            password,
-        });
+      const response = await axios.post('http://localhost:3000/auth/login', {
+        username,
+        password,
+      });
 
-        const token = response.data.token;
+      const token = response.data.token;
 
-        localStorage.setItem('token', token);
+      toast({
+        title: 'Erfolgreich',
+        description: 'Login erfolgreich',
+        status: 'success',
+      });
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } catch (err: any) {
-        if (err.ErrorRest) {
-            alert("Failed login: " + err.ErrorRest)
-        }
+      // Fügen Sie hier Logik hinzu, um mit dem Token umzugehen, z. B. das Speichern im lokalen Speicher.
+    } catch (error) {
+      toast({
+        title: 'Fehler',
+        description: 'Login fehlgeschlagen',
+        status: 'error',
+      });
     }
-  }
+  };
 
   return (
     <div>
-      <h1>Login</h1>
-      <input
-        type="text"
-        value={username}
-        placeholder="Username"
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <br />
-      <input
-        type="password"
-        value={password}
-        placeholder="Password"
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <br />
-      <button onClick={login}>
-        Login
-      </button>
+      <Center>
+        <Box w="400px" h="auto" textAlign="center">
+          <Heading marginBottom="2">Login</Heading>
+
+          <Input
+            type="text"
+            value={username}
+            variant='outline'
+            placeholder='Username'
+            width='auto'
+            onChange={(e) => setUsername(e.target.value)}
+          />
+
+          <Input
+            type="password"
+            value={password}
+            variant='outline'
+            placeholder='Password'
+            width='auto'
+            marginBottom="2"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <br></br>
+          <Button onClick={handleLogin}>
+            Login
+          </Button>
+        </Box>
+      </Center>
     </div>
   );
 };
