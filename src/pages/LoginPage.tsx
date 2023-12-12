b5c86722a2d133bdf50a9b4a6e1d77f7082b8b1d
Cao Duong <113590257+ngca1011@users.noreply.github.com>
2023-12-12 12:36:30 +0100

    Loginseite anpassen

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,33 +1,66 @@
 import { useState } from "react";
 import axios from 'axios';
-import {Stack, Button, Input, FormControl, Flex, Text} from '@chakra-ui/react'
+import {Stack, Button, Input, FormControl, Flex, Text, Box, Alert, AlertDescription, AlertIcon, CircularProgress} from '@chakra-ui/react'
+
+const ErrorMessage = ({message}) => {
+  return (
+    <Box my = {4}>
+       <Alert status = 'error' borderRadius = {4}>
+          <AlertIcon />
+          <AlertDescription>{message}</AlertDescription>
+       </Alert>
+    </Box>
+  )
+}
 
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-
-  const login = async () => {
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const handleSubmit = async (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+    setIsLoading(true);
     try {
-        const response = await axios.post('http://localhost:3000/api/auth/login', {
-            username,
-            password,
-        });
-
-        const token = response.data.token;
-
-        localStorage.setItem('token', token);
-
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } catch (err: any) {
-        if (err.ErrorRest) {
-            alert("Failed login: " + err.ErrorRest);
+      const response = await axios.post('https://localhost:3000/auth/login', {
+        auth:{
+          username,
+          password,
         }
+      });
+
+      const token = response.data.token;
+      localStorage.setItem('token', token);
+      
+      setIsLoading(false);
+      setIsLoggedIn(true);
+    }
+    catch (err) {
+      setError("Invalid username or password");
+      setIsLoading(false);
+      setUsername('');
+      setPassword('');
     }
   }
 
   return (
    <Flex align = "center" justify="center">
-    <form action = 'submit'>
+    {isLoggedIn? (
+      <Box textAlign='center'>
+        <Text>{username} logged in!</Text>
+      <Button
+        variant="outline"
+        width="full"
+        mt={4}
+        onClick={() => setIsLoggedIn(false)}
+      >
+      Sign out
+      </Button> 
+    </Box>
+    ) : ( 
+    <form onSubmit = {handleSubmit}>
+      {error && <ErrorMessage message = {error} />}
       <Text as="h1" fontSize="2xl" fontWeight="bold" align = "center">
         Login
       </Text>
@@ -38,21 +71,29 @@ const Login = () => {
             value={username}
             placeholder="Username"
             aria-label = "Username"
-            onChange={(e) => setUsername(e.target.value)}
+            onChange={event => setUsername(event.target.value)}
           />
           <Input
             type="password"
             value={password}
             placeholder="Password"
             aria-labbel = "Password"
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={event => setPassword(event.target.value)}
           />
         </FormControl>
-        <Button onClick={login} type = 'submit' variant='solid' borderColor = 'black'>
-         Sign up!
+        <Button type = 'submit' variant='solid' borderColor = 'black'>
+          {isLoading ? (
+            <CircularProgress 
+              isIndeterminate
+              color = 'teal'
+            />
+          ) : (
+            'Sign up!'
+          )}
         </Button>
       </Stack>
     </form>
+    )}
   </Flex>
   );
 };
