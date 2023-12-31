d07b2fd5b956ac4f92b6964cca6593515a70f3db
Julius Moeckel <julius.moeckel@web.de>
2023-12-31 15:12:28 +0100

    style: Interfaces auslagern

---
+++ src/types/interfaces.ts
@@ -0,0 +1,28 @@
+export interface Book {
+    isbn: string
+    rating: number
+    art: string
+    preis: number
+    rabatt: number
+    lieferbar: boolean
+    datum: string
+    homepage: string
+    schlagwoerter: string[]
+    titel: {
+      titel: string
+      untertitel: string
+    }
+    _links: {
+      self: {
+        href: string
+      }
+    }
+  }
+  
+  export interface Embedded {
+    buecher: Book[]
+  }
+  
+  export interface FetchedData {
+    _embedded: Embedded
+  }
