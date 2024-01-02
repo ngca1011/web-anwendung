d4d7e7b0fcfde52f9c79042928365a9f48e69018
Cao Nguyen Duong Nguyen <caond.nguyen@gmail.com>
2024-01-02 17:42:36 +0100

    refractoring : CRLF zu LF

--- src/pages/BuchSuchenPage.tsx
+++ src/pages/BuchSuchenPage.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import type { Book, FetchedData } from '../types/interfaces'; // Import types using 'import type'
+import type { Book, FetchedData } from '../types/interfaces' // Import types using 'import type'
 
 import {
   FormHelperText,
@@ -46,7 +46,7 @@ const Buchsuchen = () => {
   const [lieferbarValue, setLieferbarValue] = useState<string>('') // Neue State-Variable für Lieferbar
   const [selectedBook, setSelectedBook] = useState<Book | null>(null) // Zustand für ausgewähltes Buch
 
-  const [fetchedData, setFetchedData] = useState<FetchedData>({ _embedded: { buecher: [] } });
+  const [fetchedData, setFetchedData] = useState<FetchedData>({ _embedded: { buecher: [] } })
   useEffect(() => {
     const fetchDataFromApi = async () => {
       try {
--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -1,28 +1,28 @@
 export interface Book {
-    isbn: string
-    rating: number
-    art: string
-    preis: number
-    rabatt: number
-    lieferbar: boolean
-    datum: string
-    homepage: string
-    schlagwoerter: string[]
-    titel: {
-      titel: string
-      untertitel: string
+  isbn: string
+  rating: number
+  art: string
+  preis: number
+  rabatt: number
+  lieferbar: boolean
+  datum: string
+  homepage: string
+  schlagwoerter: string[]
+  titel: {
+    titel: string
+    untertitel: string
+  }
+  _links: {
+    self: {
+      href: string
     }
-    _links: {
-      self: {
-        href: string
-      }
-    }
   }
-  
-  export interface Embedded {
-    buecher: Book[]
-  }
-  
-  export interface FetchedData {
-    _embedded: Embedded
-  }
+}
+
+export interface Embedded {
+  buecher: Book[]
+}
+
+export interface FetchedData {
+  _embedded: Embedded
+}
