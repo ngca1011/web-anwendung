91a4ffe0fcb390b5126bd12ca6542a0f511b4d3c
Cao Nguyen Duong Nguyen <caond.nguyen@gmail.com>
2024-01-03 16:33:38 +0100

    Update: Dependencies und Refractoring: Routing mit React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,32 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navigation from './components/Navbar.tsx'
 import Home from './pages/HomePage.tsx'
 import Buchsuchen from './pages/BuchSuchenPage.tsx'
 import Buchanlegen from './pages/BuchCreatePage.tsx'
 import Login from './pages/LoginPage.tsx'
 import { AuthProvider } from './components/Auth.tsx'
-import React from 'react'
 
 function App() {
-  let Component: React.ComponentType = Home
-
-  switch (window.location.pathname) {
-    case '/':
-      Component = Home
-      break
-    case '/buchsuchen':
-      Component = Buchsuchen
-      break
-    case '/buchanlegen':
-      Component = Buchanlegen
-      break
-    case '/anmelden':
-      Component = Login
-  }
   return (
     <AuthProvider>
-      <div
-        style={{
-          backgroundImage: 'url("../public/background.jpg")',
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          minHeight: '100vh',
-        }}
-      >
-        <Navigation />
-        <Component />
-      </div>
+      <BrowserRouter>
+        <div
+          style={{
+            backgroundImage: 'url("../public/background.jpg")',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            minHeight: '100vh',
+          }}
+        >
+          <Navigation />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/buchsuchen' Component={Buchsuchen} />
+            <Route path='/buchanlegen' Component={Buchanlegen} />
+            <Route path='/anmelden' Component={Login} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </AuthProvider>
   )
 }
