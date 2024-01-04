724f219ad7405e7cb7c48218cf3d0edf51d8565f
Valentin Sackmann <sackmannva@gmail.com>
2024-01-04 17:16:36 +0100

    TableContainer durch SimpleGrid ersetzen

--- src/pages/BuchCreatePage.tsx
+++ src/pages/BuchCreatePage.tsx
@@ -1,12 +1,4 @@
 import {
-  Table,
-  Thead,
-  Tbody,
-  Tfoot,
-  Tr,
-  Th,
-  Td,
-  TableContainer,
   Box,
   FormControl,
   Input,
@@ -24,6 +16,7 @@ import {
   NumberIncrementStepper,
   NumberDecrementStepper,
   FormErrorMessage,
+  SimpleGrid,
 } from '@chakra-ui/react'
 import axios from 'axios'
 import { useState } from 'react'
@@ -60,6 +53,13 @@ const Buchanlegen = () => {
 
   const handleArtChange = (value: string) => {
     setArt(value)
+  }
+
+  const commonBoxStyles = {
+    borderWidth: '1px',
+    borderRadius: 'md',
+    overflow: 'hidden',
+    boxShadow: 'md',
   }
 
   const payload = {
@@ -177,218 +177,224 @@ const Buchanlegen = () => {
         minHeight: '100vh',
       }}
     >
-      <TableContainer>
-        <Table variant='simple'>
-          <Thead>
-            <Tr>
-              <Th>Kriterium</Th>
-              <Th>Eingabe</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            <Tr>
-              <Td>Titel</Td>
-              <Td>
-                <Box maxW='300px'>
-                  <FormControl isInvalid={titelError !== ''}>
-                    <Input
-                      name='titel'
-                      value={titel}
-                      onChange={(e) => {
-                        handleTitelChange(e.target.value)
-                      }}
-                    />
-                    <FormErrorMessage>{titelError}</FormErrorMessage>
-                  </FormControl>
-                </Box>
-              </Td>
-            </Tr>
-            <Tr>
-              <Td>ISBN Nummer</Td>
-              <Td>
-                <Box maxW='300px'>
-                  <FormControl isInvalid={isbnError !== ''}>
-                    <Input
-                      name='isbn'
-                      value={isbn}
-                      onChange={(e) => {
-                        handleIsbnChange(e.target.value)
-                      }}
-                    />
-                    <FormErrorMessage>{isbnError}</FormErrorMessage>
-                  </FormControl>
-                </Box>
-              </Td>
-            </Tr>
-            <Tr>
-              <Td>Buchart</Td>
-              <Td>
-                <Box mb={4}>
-                  <FormControl>
-                    <Stack direction='row'>
-                      <Checkbox
-                        name='art'
-                        isChecked={art === 'DRUCKAUSGABE'}
-                        onChange={() => {
-                          handleArtChange('DRUCKAUSGABE')
-                        }}
-                      >
-                        Druckausgabe
-                      </Checkbox>
-                      <Checkbox
-                        name='isKindle'
-                        isChecked={art === 'KINDLE'}
-                        onChange={() => {
-                          handleArtChange('KINDLE')
-                        }}
-                      >
-                        Kindle
-                      </Checkbox>
-                    </Stack>
-                  </FormControl>
-                </Box>
-              </Td>
-            </Tr>
-            <Tr>
-              <Td>Rating</Td>
-              <Td>
-                <Box maxW='300px'>
-                  <Menu>
-                    <MenuButton as={Button}>{rating ?? 'Choose'}</MenuButton>
-                    <MenuList>
-                      {[1, 2, 3, 4, 5].map((value) => (
-                        <MenuItem
-                          key={value}
-                          onClick={() => {
-                            setRating(value)
-                          }}
-                        >
-                          {value}
-                        </MenuItem>
-                      ))}
-                    </MenuList>
-                  </Menu>
-                </Box>
-              </Td>
-            </Tr>
-            <Tr>
-              <Td>Lieferbar</Td>
-              <Td>
-                <Box mb={4}>
-                  <FormControl>
-                    <Stack direction='row'>
-                      <Checkbox
-                        name='lieferbar'
-                        isChecked={lieferbar === 'true'}
-                        onChange={() => {
-                          setLieferbar('true')
-                        }}
-                      >
-                        Ja
-                      </Checkbox>
-                      <Checkbox
-                        name='lieferbar'
-                        isChecked={lieferbar === 'false'}
-                        onChange={() => {
-                          setLieferbar('false')
-                        }}
-                      >
-                        Nein
-                      </Checkbox>
-                    </Stack>
-                  </FormControl>
-                </Box>
-              </Td>
-            </Tr>
-            <Tr>
-              <Td>Preis</Td>
-              <Td>
-                <Box maxW='300px'>
-                  <FormControl>
-                    <NumberInput
-                      defaultValue={15}
-                      precision={2}
-                      min={0}
-                      onChange={(valueString) => {
-                        setPreisString(parsePreis(valueString))
-                      }}
-                      value={formatPreis(preisString)}
-                    >
-                      <NumberInputField />
-                      <NumberInputStepper>
-                        <NumberIncrementStepper />
-                        <NumberDecrementStepper />
-                      </NumberInputStepper>
-                    </NumberInput>
-                  </FormControl>
-                </Box>
-              </Td>
-            </Tr>
-            <Tr>
-              <Td>Rabatt</Td>
-              <Td>
-                <Box maxW='300px'>
-                  <FormControl>
-                    <NumberInput
-                      defaultValue={0}
-                      min={0}
-                      max={1}
-                      step={0.1}
-                      onChange={(valueString) => {
-                        setRabattString(valueString)
-                      }}
-                      value={rabattString}
-                    >
-                      <NumberInputField />
-                      <NumberInputStepper>
-                        <NumberIncrementStepper />
-                        <NumberDecrementStepper />
-                      </NumberInputStepper>
-                    </NumberInput>
-                  </FormControl>
-                </Box>
-              </Td>
-            </Tr>
-            <Tr>
-              <Td>Datum</Td>
-              <Td>
-                <Box maxW='300px'>
-                  <FormControl>
-                    <input
-                      type='date'
-                      name='datum'
-                      value={datum}
-                      max={today}
-                      onChange={(valueString) => {
-                        setDatum(valueString.target.value)
-                      }}
-                    />
-                  </FormControl>
-                </Box>
-              </Td>
-            </Tr>
-            <Tr>
-              <Td>Homepage</Td>
-              <Td>
-                <Box maxW='300px'>
-                  <FormControl isInvalid={homepageError !== ''}>
-                    <Input
-                      name='homepage'
-                      value={homepage}
-                      onChange={(e) => {
-                        handleHomepageChange(e.target.value)
-                      }}
-                    />
-                    <FormErrorMessage>{homepageError}</FormErrorMessage>
-                  </FormControl>
-                </Box>
-              </Td>
-            </Tr>
-          </Tbody>
-          <Tfoot>{/* Optionaler Tabellenfuß */}</Tfoot>
-        </Table>
-      </TableContainer>
+      <SimpleGrid columns={2} spacing={4} width='800px' margin='auto'>
+        <Box style={commonBoxStyles}>
+          <Box padding={4} borderRightWidth={1} fontWeight='bold'>
+            Titel
+          </Box>
+          <Box padding='4'>
+            {' '}
+            <FormControl isInvalid={titelError !== ''}>
+              <Input
+                name='titel'
+                value={titel}
+                onChange={(e) => {
+                  handleTitelChange(e.target.value)
+                }}
+              />
+              <FormErrorMessage>{titelError}</FormErrorMessage>
+            </FormControl>
+          </Box>
+        </Box>
 
+        <Box style={commonBoxStyles}>
+          <Box padding={4} borderRightWidth={1} fontWeight='bold'>
+            ISBN Number
+          </Box>
+
+          <Box padding='4'>
+            <FormControl isInvalid={isbnError !== ''}>
+              <Input
+                name='isbn'
+                value={isbn}
+                onChange={(e) => {
+                  handleIsbnChange(e.target.value)
+                }}
+              />
+              <FormErrorMessage>{isbnError}</FormErrorMessage>
+            </FormControl>
+          </Box>
+        </Box>
+
+        <Box style={commonBoxStyles}>
+          <Box padding={4} borderRightWidth={1} fontWeight='bold'>
+            Rating
+          </Box>
+
+          <Box padding='4'>
+            <Menu>
+              <MenuButton as={Button}>{rating ?? 'Choose'}</MenuButton>
+              <MenuList>
+                {[1, 2, 3, 4, 5].map((value) => (
+                  <MenuItem
+                    key={value}
+                    onClick={() => {
+                      setRating(value)
+                    }}
+                  >
+                    {value}
+                  </MenuItem>
+                ))}
+              </MenuList>
+            </Menu>
+          </Box>
+        </Box>
+
+        <Box style={commonBoxStyles}>
+          <Box padding={4} borderRightWidth={1} fontWeight='bold'>
+            Buchart
+          </Box>
+
+          <Box padding='4'>
+            <FormControl>
+              <Stack direction='row'>
+                <Checkbox
+                  name='art'
+                  isChecked={art === 'DRUCKAUSGABE'}
+                  onChange={() => {
+                    handleArtChange('DRUCKAUSGABE')
+                  }}
+                >
+                  Druckausgabe
+                </Checkbox>
+                <Checkbox
+                  name='isKindle'
+                  isChecked={art === 'KINDLE'}
+                  onChange={() => {
+                    handleArtChange('KINDLE')
+                  }}
+                >
+                  Kindle
+                </Checkbox>
+              </Stack>
+            </FormControl>
+          </Box>
+        </Box>
+
+        <Box style={commonBoxStyles}>
+          <Box padding={4} borderRightWidth={1} fontWeight='bold'>
+            Lieferbar
+          </Box>
+
+          <Box padding='4'>
+            <FormControl>
+              <Stack direction='row'>
+                <Checkbox
+                  name='lieferbar'
+                  isChecked={lieferbar === 'true'}
+                  onChange={() => {
+                    setLieferbar('true')
+                  }}
+                >
+                  Ja
+                </Checkbox>
+                <Checkbox
+                  name='lieferbar'
+                  isChecked={lieferbar === 'false'}
+                  onChange={() => {
+                    setLieferbar('false')
+                  }}
+                >
+                  Nein
+                </Checkbox>
+              </Stack>
+            </FormControl>
+          </Box>
+        </Box>
+
+        <Box style={commonBoxStyles}>
+          <Box padding={4} borderRightWidth={1} fontWeight='bold'>
+            Preis
+          </Box>
+
+          <Box padding='4'>
+            <FormControl>
+              <NumberInput
+                defaultValue={15}
+                precision={2}
+                min={0}
+                onChange={(valueString) => {
+                  setPreisString(parsePreis(valueString))
+                }}
+                value={formatPreis(preisString)}
+              >
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+            </FormControl>
+          </Box>
+        </Box>
+
+        <Box style={commonBoxStyles}>
+          <Box padding={4} borderRightWidth={1} fontWeight='bold'>
+            Rabatt
+          </Box>
+          <Box padding='4'>
+            {' '}
+            <FormControl>
+              <NumberInput
+                defaultValue={0}
+                min={0}
+                max={1}
+                step={0.1}
+                onChange={(valueString) => {
+                  setRabattString(valueString)
+                }}
+                value={rabattString}
+              >
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+            </FormControl>
+          </Box>
+        </Box>
+
+        <Box style={commonBoxStyles}>
+          <Box padding={4} borderRightWidth={1} fontWeight='bold'>
+            Datum
+          </Box>
+
+          <Box padding='4'>
+            {' '}
+            <FormControl>
+              <input
+                type='date'
+                name='datum'
+                value={datum}
+                max={today}
+                onChange={(valueString) => {
+                  setDatum(valueString.target.value)
+                }}
+              />
+            </FormControl>
+          </Box>
+        </Box>
+        <Box style={commonBoxStyles}>
+          <Box padding={4} borderRightWidth={1} fontWeight='bold'>
+            Homepage
+          </Box>
+
+          <Box padding='4'>
+            {' '}
+            <FormControl isInvalid={homepageError !== ''}>
+              <Input
+                name='homepage'
+                value={homepage}
+                onChange={(e) => {
+                  handleHomepageChange(e.target.value)
+                }}
+              />
+              <FormErrorMessage>{homepageError}</FormErrorMessage>
+            </FormControl>
+          </Box>
+        </Box>
+      </SimpleGrid>
       <Box display='flex' justifyContent='center' alignItems='center' marginBottom='4'>
         <Button
           onClick={() => {
